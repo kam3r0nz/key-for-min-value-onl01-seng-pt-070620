900cdf66708fa7b99c4fda5c33ee35b52b016e04
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-16 01:23:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if low_value == 0 || value < low_value
       low_value = value
+      low_key = key
     else
       key = key.sort_by {|k, v| v}
       key[0]
